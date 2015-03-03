f700aa0359d5cf668c657a182ae585dfb2be2004
Thomas Watson Steen <w@tson.dk>
2015-03-03 21:56:09 +0100

    Add support for setting custom envionment

--- test/index.js
+++ test/index.js
@@ -17,7 +17,7 @@ test('run with non-existing hook', function (t) {
   });
 });
 
-test('run with dir string', function (t) {
+test('init with dir string', function (t) {
   var hooks = Hooks(dir);
   hooks('foo', function (hook) {
     var stdout = [];
@@ -33,7 +33,7 @@ test('run with dir string', function (t) {
   });
 });
 
-test('run with opts.dir', function (t) {
+test('init with opts.dir', function (t) {
   var hooks = Hooks({ dir: dir });
   hooks('foo', function (hook) {
     var stdout = [];
@@ -49,7 +49,7 @@ test('run with opts.dir', function (t) {
   });
 });
 
-test('run with opts.dir + opts.cwd', function (t) {
+test('init with opts.dir + opts.cwd', function (t) {
   var cwd = fs.realpathSync(os.tmpdir());
   var hooks = Hooks({ dir: dir, cwd: cwd });
   hooks('foo', function (hook) {
@@ -66,7 +66,39 @@ test('run with opts.dir + opts.cwd', function (t) {
   });
 });
 
-test('run with override', function (t) {
+test('init with opts.dir + opts.env', function (t) {
+  var hooks = Hooks({ dir: dir, env: { FOO: 'bar' } });
+  hooks('foo', function (hook) {
+    var stdout = [];
+    var stderr = [];
+    hook.on('close', function (code) {
+      t.equals(code, 42);
+      t.equals(stdout.join(''), 'bar' + cmd);
+      t.equals(stderr.join(''), process.cwd());
+      t.end();
+    });
+    hook.stdout.on('data', stdout.push.bind(stdout));
+    hook.stderr.on('data', stderr.push.bind(stderr));
+  });
+});
+
+test('init with dir and run with empty args and opts.env', function (t) {
+  var hooks = Hooks(dir);
+  hooks('foo', [], { env: { FOO: 'bar' } }, function (hook) {
+    var stdout = [];
+    var stderr = [];
+    hook.on('close', function (code) {
+      t.equals(code, 42);
+      t.equals(stdout.join(''), 'bar' + cmd);
+      t.equals(stderr.join(''), process.cwd());
+      t.end();
+    });
+    hook.stdout.on('data', stdout.push.bind(stdout));
+    hook.stderr.on('data', stderr.push.bind(stderr));
+  });
+});
+
+test('init with override', function (t) {
   var hooks = Hooks({ dir: dir, overrides: { foo: 'echo bar' } });
   hooks('foo', function (hook) {
     var stdout = [];
